9b2778c9199f6f1d22910c7163c5a81e6840a78f
Sneha Mehta <snehamehta@vt.edu>
2018-04-17 15:54:30 -0400

    add CNN model

---
+++ model_CNN.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class CNN_Text(nn.Module):
+
+    def __init__(self, ntok, ninp, nclass, emb_matrix, nkernel=100, ksizes=[3, 4, 5], static=False, drop=0.5 ):
+        super(CNN_Text, self).__init__()
+        V = ntok
+        D = ninp
+        C = nclass
+        Ci = 1
+        Co = nkernel
+        Ks = ksizes
+
+        self.static = static
+        self.embed = nn.Embedding( V, D )
+        # self.convs1 = [nn.Conv2d(Ci, Co, (K, D)) for K in Ks]
+        self.convs1 = nn.ModuleList([nn.Conv2d(Ci, Co, (K, D)) for K in Ks])
+        '''
+        self.conv13 = nn.Conv2d(Ci, Co, (3, D))
+        self.conv14 = nn.Conv2d(Ci, Co, (4, D))
+        self.conv15 = nn.Conv2d(Ci, Co, (5, D))
+        '''
+        self.dropout = nn.Dropout(drop)
+        self.fc1 = nn.Linear(len(Ks)*Co, C)
+
+        self.init_wordembedding( emb_matrix )
+
+    def init_wordembedding( self, embedding_matrix ):
+        self.embed.weight.data = embedding_matrix
+
+    def conv_and_pool(self, x, conv):
+        x = F.relu(conv(x)).squeeze(3)  # (N, Co, W)
+        x = F.max_pool1d(x, x.size(2)).squeeze(2)
+        return x
+
+    def forward(self, x):
+        x = self.embed(x)  # (N, W, D)
+
+        if self.static:
+            x = Variable(x)
+
+        x = x.unsqueeze(1)  # (N, Ci, W, D)
+
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1]  # [(N, Co, W), ...]*len(Ks)
+
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N, Co), ...]*len(Ks)
+
+        x = torch.cat(x, 1)
+
+        '''
+        x1 = self.conv_and_pool(x,self.conv13) #(N,Co)
+        x2 = self.conv_and_pool(x,self.conv14) #(N,Co)
+        x3 = self.conv_and_pool(x,self.conv15) #(N,Co)
+        x = torch.cat((x1, x2, x3), 1) # (N,len(Ks)*Co)
+        '''
+        x = self.dropout(x)  # (N, len(Ks)*Co)
+        logit = self.fc1(x)  # (N, C)
+        return logit
